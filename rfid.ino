9d4d9ad3bd2839f7d646defbb33e4734378067ad
KH_jinu <kevinlike@naver.com>
2023-04-25 21:22:14 +0900

    함수 정리

--- rfid.ino
+++ rfid.ino
@@ -13,6 +13,29 @@ void RfidInit(){
   Serial.println("Waiting for Card ...");
 }
 
+void RfidCheck(uint8_t data[32]){
+  String RfidID = "";
+
+  for(int i=0; i<4; i++){
+    RfidID += (char)data[i];
+  }
+
+  if(RfidID == "G1P9")
+  {
+    Serial.print("G1P9");
+    CorrectRev = 10;
+  }
+  else if(RfidID == "G1P7")
+  {
+    Serial.print("G1P7");
+    CorrectRev = 5;
+  }
+  else
+    Serial.print("Unidentified Chip");
+
+  Serial.println("");
+}
+
 void RfidLoop(){
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
